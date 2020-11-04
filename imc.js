afb254d3644270918e5be86034a3f234b5d63470
Bernard Telles <bernardtelgon@gmail.com>
2020-11-04 20:11:04 -0300

    imc calculator

---
+++ imc.js
@@ -0,0 +1,9 @@
+const nome = document.getElementById('nome');
+const altura = document.getElementById('altura');
+const peso = document.getElementById('peso');
+const calcular = document.getElementById('calcular');
+const resultado = document.getElementById('resultado');
+
+alert(nome);
+alert(altura);
+alert(altura);
