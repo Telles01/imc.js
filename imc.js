afc544ea96aa47a8bf9d918c30fd1f4a4b0b9b85
Bernard Telles <bernardtelgon@gmail.com>
2020-11-04 23:42:26 -0300

    finish calculator

--- imc.js
+++ imc.js
@@ -10,8 +10,29 @@ function imc () {
   const resultado = document.getElementById('resultado');
 
   if(nome !== '' && altura !== '' && peso !== '') {
-      alert ('Preencha todos os campos')
-  }else 
+
+      const valorIMC = (peso / (altura * altura)).toFixed(2);
+
+      let classificado = '';
+
+      if(valorIMC < 18.5){
+        classificacao = 'abaixo do peso.';
+      }else if (valorIMC < 25) {
+        classificacao = 'com o peso ideal.';
+      }else if (valorIMC < 30){
+        classificacao = 'levemente acima do peso.';
+      }else if (valorIMC < 35){
+        classificacao = 'com obesidade grau 1.';
+      }else if (valorIMC < 40){
+        classificacao = 'com obesidade grau 2';
+      }else if (valorIMC < 45){
+        classificacao = 'com obesidade grau 3';
+      }
+
+      resultado.textContent = `${nome} seu IMC é ${valorIMC} e voce está ${classificacao} `;
+  }else{
+      resultado.textContent = 'Preencha todos os campos!!';
+  } 
 
 }
 
